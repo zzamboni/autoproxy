72666123f876b9221309628a7b0bd8413d768ea7
Diego Zamboni <diego@zzamboni.org>
2017-03-07 09:00:47 +0100

    Remove functions on uninstall

--- uninstall.fish
+++ uninstall.fish
@@ -2,3 +2,4 @@
 #
 # You can use this file to do custom cleanup when the package is uninstalled.
 # You can use the variable $path to access the package path.
+functions -e autoproxy __autoproxy_notif_requested
