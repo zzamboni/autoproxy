5a2d855af2784261f9bcf2fc64a20200bb473068
Diego Zamboni <diego@zzamboni.org>
2017-03-07 08:41:49 +0100

    Simplified and cleaned up code

--- functions/autoproxy.fish
+++ functions/autoproxy.fish
@@ -1,21 +1,19 @@
 function autoproxy -d "Automatically set/unset proxy" --on-event fish_prompt
-        if set -q autoproxy_check_command
-                if eval $autoproxy_check_command
-                        if set -q autoproxy_notifications; and test $autoproxy_notifications = "yes"
-                                if not set -q __autoproxy_set
-                                        set_color yellow; echo "Setting proxy"; set_color normal
-                                end
-                        end
-                        proxy
-                        set -g __autoproxy_set
-                else
-                        if set -q autoproxy_notifications; and test $autoproxy_notifications = "yes"
-                                if set -q __autoproxy_set
-		                        set_color green; echo "Unsetting proxy"; set_color normal
-                                end
-                        end
-                        noproxy
-                        set -e __autoproxy_set
-                end
-        end
+  fish_debug "autoproxy: entering autoproxy function"
+    if set -q autoproxy_check_command
+      # echo ... | source instead of "eval" to avoid a warning from exec-path-from-shell in Emacs
+      if echo $autoproxy_check_command | source
+        if __autoproxy_notif_requested; and not set -q __autoproxy_set
+        fish_info "autoproxy: Setting proxy"
+        proxy
+        set -U __autoproxy_set
+      end
+    else
+      if __autoproxy_notif_requested; and set -q __autoproxy_set
+		    fish_info "autoproxy: Unsetting proxy"
+        noproxy
+        set -e __autoproxy_set
+      end
+    end
+  end
 end
