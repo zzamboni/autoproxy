54da4896f78b8cd5fbc5f05ec69bcf13c08d1a61
Diego Zamboni <diego@zzamboni.org>
2017-03-07 08:49:16 +0100

    Fix syntax error and logic bug in setting/unsetting proxy

--- functions/autoproxy.fish
+++ functions/autoproxy.fish
@@ -4,16 +4,16 @@ function autoproxy -d "Automatically set/unset proxy" --on-event fish_prompt
       # echo ... | source instead of "eval" to avoid a warning from exec-path-from-shell in Emacs
       if echo $autoproxy_check_command | source
         if __autoproxy_notif_requested; and not set -q __autoproxy_set
-        fish_info "autoproxy: Setting proxy"
+          fish_info "autoproxy: Setting proxy"
+        end
         proxy
         set -U __autoproxy_set
-      end
-    else
-      if __autoproxy_notif_requested; and set -q __autoproxy_set
-		    fish_info "autoproxy: Unsetting proxy"
+      else
+        if __autoproxy_notif_requested; and set -q __autoproxy_set
+		      fish_info "autoproxy: Unsetting proxy"
+        end
         noproxy
         set -e __autoproxy_set
       end
     end
-  end
-end
+  end 
