b5f5fec2999b1f8620c30c4c772983798b499796
Diego Zamboni <diego@zzamboni.org>
2017-03-08 21:04:54 +0100

    Fix return code from autoproxy: return 0 if proxy was set, non-zero otherwise

--- functions/autoproxy.fish
+++ functions/autoproxy.fish
@@ -2,7 +2,9 @@ function autoproxy -d "Automatically set/unset proxy" --on-event fish_prompt
   fish_debug "autoproxy: entering autoproxy function"
     if set -q autoproxy_check_command
       # echo ... | source instead of "eval" to avoid a warning from exec-path-from-shell in Emacs
-      if echo $autoproxy_check_command | source
+      echo $autoproxy_check_command | source
+      set result $status
+      if test $result -eq 0
         if __autoproxy_notif_requested; and not set -q __autoproxy_set
           fish_info "autoproxy: Setting proxy"
         end
@@ -10,10 +12,11 @@ function autoproxy -d "Automatically set/unset proxy" --on-event fish_prompt
         set -U __autoproxy_set
       else
         if __autoproxy_notif_requested; and set -q __autoproxy_set
-		      fish_info "autoproxy: Unsetting proxy"
+          fish_info "autoproxy: Unsetting proxy"
         end
         noproxy
         set -e __autoproxy_set
       end
     end
+    return $result
   end 
